013c279ed89e7fd83a41221399062033d90e7f00
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-04-22 12:54:14 +0700

    pembeharuann javascript tentang section

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 // document.getElementById()
 
 const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Rizwan Gustama';
+
+// document.getElementByTagName()
+// -> HTMLCollections
+const p = document.getElementsByTagName('p');
+// p[2].style.backgroundColor ='lightblue';
+
+// Atau bisa menggunakan looping
+
+for( let i = 0; i < p.length; i++ ) {
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '45px';
+
+// document.getElementByClassName()
+// -> HTMLCollections
+
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = "Ini diubah dari Javascript";
