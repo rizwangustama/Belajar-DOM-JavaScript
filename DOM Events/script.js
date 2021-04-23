a40e6af84d4e95d8dc231aefe4365da032d53c60
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-04-23 15:31:14 +0700

    pembaharuan

--- DOM Events/script.js
+++ DOM Events/script.js
@@ -5,4 +5,46 @@
 // 2. addEventListener()
 // ***********************************
 
+const p3 = document.querySelector('.p3');
 
+function ubahWarnaP3() {
+    p3.style.backgroundColor = 'lightblue';
+}
+
+function ubahWarnaP2() {
+    p2.style.backgroundColor = 'lightblue';
+}
+
+
+
+const p2 = document.querySelector('.p2');
+p2.onclick = ubahWarnaP2;
+
+
+
+// 2. addEventListener()
+
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click', function() {
+    const ul = document.querySelector('section#b ul');
+
+    const li = document.createElement('li');
+    const litext = document.createTextNode('List Baru');
+
+    li.appendChild(litext);
+    ul.appendChild(li);
+
+});
+
+const h1 = document.getElementById('judul');
+h1.addEventListener('click', function() {
+    const sBaru = document.querySelector('section#a');
+
+    const pBaru = document.createElement('p');
+    const pText = document.createTextNode('paragraf Baru');
+
+    pBaru.appendChild(pText);
+    sBaru.appendChild(pBaru);
+
+
+});
