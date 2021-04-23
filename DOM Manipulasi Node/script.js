cd30ecf1b76994351a8566dbdc2f55cc902a4197
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-04-23 10:59:30 +0700

    pembaharuan

--- DOM Manipulasi Node/script.js
+++ DOM Manipulasi Node/script.js
@@ -21,17 +21,17 @@ const textFarag = document.createTextNode('Halaman Akhir');
 
 pafagB.appendChild(textFarag);
 
-const selecB = document.getElementById('b');
-selecB.appendChild(pafagB);
+// const selecB = document.getElementById('b');
+// selecB.appendChild(pafagB);
 
 
-const linkBawah = document.createElement('p');
-const textLink = document.createTextNode('Rizwan Gustama');
+// const linkBawah = document.createElement('p');
+// const textLink = document.createTextNode('Rizwan Gustama');
 
-linkBawah.appendChild(textLink);
+// linkBawah.appendChild(textLink);
 
-const sB = document.getElementById('b');
-sB.appendChild(linkBawah);
+// const sB = document.getElementById('b');
+// sB.appendChild(linkBawah);
 
 
 // const liBaru = document.createElement('li');
@@ -55,6 +55,25 @@ const ul = document.querySelector('section#b ul');
 const li3 = ul.querySelector('li:nth-child(3)');
 
 ul.insertBefore(libag2,li3);
+
+
+const link = document.getElementsByTagName('a')[0];
+sectionA.removeChild(link);
+
+
+
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const textH2Baru = document.createTextNode('Judul Baru');
+h2Baru.appendChild(textH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+h2Baru.style.backgroundColor = 'lightgreen';
+libag2.style.backgroundColor = 'lightgreen';
+pBaru.style.backgroundColor = 'lightgreen';
 
 
 
