33621059d829912997cb7ef1f9db697db78e9837
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-04-23 10:11:48 +0700

    pembahruan scriapt

--- DOM Manipulasi Node/script.js
+++ DOM Manipulasi Node/script.js
@@ -7,3 +7,31 @@
 // parentsNode.replaceChild()
 
 
+const pBaru = document.createElement('p')
+const textPBaru = document.createTextNode('Paragraf baru');
+
+// Simpan Tulisan Ke dalam Paragraf
+pBaru.appendChild(textPBaru);
+// Simpan PBaru di akhir Section A
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+const pafagB = document.createElement('p');
+const textFarag = document.createTextNode('Halaman Akhir');
+
+pafagB.appendChild(textFarag);
+
+const selecB = document.getElementById('b');
+selecB.appendChild(pafagB);
+
+
+const linkBawah = document.createElement('p');
+const textLink = document.createTextNode('Rizwan Gustama');
+
+linkBawah.appendChild(textLink);
+
+const sB = document.getElementById('b');
+sB.appendChild(linkBawah);
+
+
+
