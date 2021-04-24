45e8560ff8450768756fce14c1a3423cf525a290
Rizwan Gustama <50127801+rizwangustama@users.noreply.github.com>
2021-04-24 16:14:17 +0700

    Pembaharuan

--- DOM Bermain Dengan Warna/script.js
+++ DOM Bermain Dengan Warna/script.js
@@ -26,6 +26,8 @@ tAcakWarna.addEventListener('click', function() {
 
 
 const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
 
 sMerah.addEventListener('input', function() {
     // console.log(sMerah.value);
@@ -36,3 +38,33 @@ sMerah.addEventListener('input', function() {
     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
 
 });
+
+sHijau.addEventListener('input', function() {
+
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sBiru.addEventListener('input', function() {
+    
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+
+document.body.addEventListener('mousemove', function(event) {
+    // Posisi Mouse
+    // console.log(event.clientY);
+    // Ukuran Browser
+    // console.log(window.innerWidth);
+
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+    const yPos = Math.round((event.clientY / window.innerWidth) * 255);
+
+    document.body.style.backgroundColor = 'rgb('+ xPos +','+ yPos +', 100)';
+
+})
